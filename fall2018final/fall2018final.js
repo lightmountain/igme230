2d4f0c04635534cf848e4e3b878959c5f20f9687
Elena Berg (RIT Student) <ecb8415@ad.rit.edu>
2018-12-06 15:17:26 -0500

    ugh

--- fall2018final/fall2018final.js
+++ fall2018final/fall2018final.js
@@ -1,3 +1,24 @@
-$(".top").click(function () {
-            $(".list").slideToggle("slow");
+let menu1 = $(".top:first");
+let menu2 = $(".top").next();
+let menu3 = $(".top").next().next();
+
+let sub1 = $(".list:first");
+let sub2 = $(".list:first").next();
+let sub3 = $(".list:first").next().next();
+
+$(".list").hide();
+
+menu1.click(function () {
+            sub1.slideToggle("slow");
         });
+
+menu2.click(function () {
+            sub2.slideToggle("slow");
+        });
+
+menu3.click(function () {
+            sub3.slideToggle("slow");
+        });
+$("input").value("article1.txt").click(function() {
+
+})
